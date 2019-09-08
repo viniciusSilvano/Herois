f5faf51394f292bb87cd6c75c8b881190532cccc
Vinícius Silvano <viniciusssf@hotmail.com>
2019-09-08 12:55:24 -0300

    removendo import desnecessário na interface IDuelo

--- stefanini/src/main/java/com/stefanini/heroi/bo/partida/factory/IDuelo.java
+++ stefanini/src/main/java/com/stefanini/heroi/bo/partida/factory/IDuelo.java
@@ -3,7 +3,6 @@ package com.stefanini.heroi.bo.partida.factory;
 import java.util.List;
 
 import com.stefanini.heroi.dto.PlacarDTO;
-import com.stefanini.heroi.model.factory.IPersonagem;
 
 public interface IDuelo {
 	List<PlacarDTO> getPlacares();
